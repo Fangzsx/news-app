4a55c2edf698877910c955bcc9baab6aa7ac8f29
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-04-30 10:35:44 +0800

    add pagination for search view

--- app/src/main/java/com/fangzsx/news_app/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/fangzsx/news_app/ui/fragments/SearchNewsFragment.kt
@@ -6,13 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import androidx.appcompat.widget.SearchView
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.fangzsx.news_app.R
 import com.fangzsx.news_app.adapters.NewsAdapter
 import com.fangzsx.news_app.databinding.FragmentSearchNewsBinding
 import com.fangzsx.news_app.ui.NewsActivity
+import com.fangzsx.news_app.util.Constants
 import com.fangzsx.news_app.util.Resource
 import com.fangzsx.news_app.viewmodels.NewsViewModel
 import com.google.android.material.snackbar.Snackbar
@@ -39,6 +42,20 @@ class SearchNewsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        newsAdapter.setOnReadMoreClickListener {
+            val bundle = Bundle()
+            bundle.putString("webview_url", it.url)
+            bundle.putString("source", it.source!!.name)
+
+            findNavController().navigate(R.id.action_searchNewsFragment_to_articleFragment,bundle)
+        }
+
+        newsAdapter.setOnSaveClickListener { article ->
+            viewModel.saveArticle(article)
+            Snackbar.make(view, "Article saved.", Snackbar.LENGTH_SHORT).show()
+        }
 
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -61,37 +78,67 @@ class SearchNewsFragment : Fragment() {
         viewModel.searchNewsResult.observe(viewLifecycleOwner){ response ->
             when(response){
                 is Resource.Success -> {
-                    //hide progressbar
+                    //hide progress bar
+                    isLoading = false
                     response.data?.let { newsResponse ->
-                        newsAdapter.differ.submitList(newsResponse.articles)
+                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+
+                        val totalPages = newsResponse.totalResults / Constants.QUERY_PAGE_SIZE + 2
+                        isLastPage = viewModel.searchNewsResultPageNumber == totalPages
                     }
+
+
                 }
 
                 is Resource.Error -> {
-                    response.message?.let{ message ->
-                        Log.e(TAG, "An error occurred. $message")
+                    isLoading = false
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured. $message")
                     }
                 }
 
                 is Resource.Loading -> {
-                    //show progress bar
+                    isLoading = true
+                    Log.e(TAG, "show progress bar")
                 }
             }
+
+        }
+    }
+    //scrolling
+    var isScrolling = false
+    var isLoading = false
+    var isLastPage = false
+
+
+    private val scrollListener = object : RecyclerView.OnScrollListener(){
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+
+
         }
 
-        setupRecyclerView()
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisible = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
 
-        newsAdapter.setOnReadMoreClickListener {
-            val bundle = Bundle()
-            bundle.putString("webview_url", it.url)
-            bundle.putString("source", it.source!!.name)
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisible + visibleItemCount >= totalItemCount
+            val isAtNotBeginning = firstVisible >= 0
+            val isMoreThanVisible = totalItemCount >= Constants.QUERY_PAGE_SIZE
 
-            findNavController().navigate(R.id.action_searchNewsFragment_to_articleFragment,bundle)
-        }
+            val shouldPaginate = isNotLoadingAndNotLastPage && isAtLastItem && isAtNotBeginning && isMoreThanVisible && isScrolling
 
-        newsAdapter.setOnSaveClickListener { article ->
-            viewModel.saveArticle(article)
-            Snackbar.make(view, "Article saved.", Snackbar.LENGTH_SHORT).show()
+            if(shouldPaginate){
+                viewModel.searchNews(binding.searchView.query.toString()) //
+                isScrolling = false
+            }
         }
     }
 
@@ -101,6 +148,7 @@ class SearchNewsFragment : Fragment() {
         binding.rvSearchResults.apply{
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@SearchNewsFragment.scrollListener)
         }
     }
 
